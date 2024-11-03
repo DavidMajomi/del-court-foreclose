a8b87fe46549de287959ea2d7d49f33e300fb52b
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-11-02 23:16:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from io import BytesIO
 import test_del_court
 
 
-
-
 output = BytesIO()
 
 
@@ -15,22 +13,23 @@ st.set_page_config(page_title="Del Data")
 
 
 st.header("Del Data")
-st.write("Enter your email and upload your resume for a review.")
+st.write("Upload your data.")
 
 
 uploaded_file=st.file_uploader("Choose a file(xlsx)",type=["xlsx"])
-
-if uploaded_file is not None:
-    st.write("File Uploaded Successfully")
-
-
         
 submit1 = st.button("Submit")
 
 
 if submit1:
+    
+    if uploaded_file is not None:
+        st.write("File Uploaded Successfully")
+    
+    
+    
     if uploaded_file:
-        st.subheader("Processing this could take up to 20 minutes depending on file size")
+        st.subheader("Processing this could take up to 20 minutes depending on file size. Please do not close or refresh this tab.")
         
         
         columns = ["Owner Last Name", "Owner First Name", "Case Number"]
@@ -59,7 +58,7 @@ if submit1:
         # print(type(xlsx_data))
         
         
-        st.download_button(label="label", data=xlsx_data, file_name="data.xlsx", mime="application/vnd.ms-excel")
+        st.download_button(label="Download Retrieved Data", data=xlsx_data, file_name="data.xlsx", mime="application/vnd.ms-excel")
         
     #st.dataframe(web_data1)
         
