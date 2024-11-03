76a904a267338e375dcc01bb9ff3b11caf4fe3dd
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-11-02 22:37:19 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pyexcel as pyxl
+import pandas as pd
+from xlsxwriter import Workbook
+from io import BytesIO
+import test_del_court
+
+
+
+
+output = BytesIO()
+
+
+st.set_page_config(page_title="Del Data")
+
+
+st.header("Del Data")
+st.write("Enter your email and upload your resume for a review.")
+
+
+uploaded_file=st.file_uploader("Choose a file(xlsx)",type=["xlsx"])
+
+if uploaded_file is not None:
+    st.write("File Uploaded Successfully")
+
+
+        
+submit1 = st.button("Submit")
+
+
+if submit1:
+    if uploaded_file:
+        st.subheader("Processing this could take up to 20 minutes depending on file size")
+        
+        
+        columns = ["Owner Last Name", "Owner First Name", "Case Number"]
+    
+        source_data = pd.read_excel(uploaded_file, usecols=columns)
+        
+        
+        
+        print(source_data)
+        
+        test_del_court.find_abnormal_size_of_case_numbers(source_data)
+        test_del_court.clean_case_numbers(source_data)
+        test_del_court.find_abnormal_size_of_case_numbers(source_data)
+        
+        web_data = test_del_court.get_new_web_case_data(source_data)
+        
+        
+        
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        web_data.to_excel(writer, sheet_name='Sheet1')
+
+        writer.close()
+        xlsx_data = output.getvalue()
+
+        # print(xlsx_data)
+        # print(type(xlsx_data))
+        
+        
+        st.download_button(label="label", data=xlsx_data, file_name="data.xlsx", mime="application/vnd.ms-excel")
+        
+    #st.dataframe(web_data1)
+        
+        
